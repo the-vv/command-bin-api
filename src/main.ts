f0d56414b9d1c9d7d4f64ef333916b4497d3eb88
Vishnu Vinod <vishnuvinod2772001@gmail.com>
2025-06-19 02:05:03 +0530

    Added CORS_ORIGIN to the environment variables and configured CORS settings in the application.

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: process.env.CORS_ORIGIN || '*', // Allow all origins by default, can be overridden by environment variable
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      credentials: true,
+    }
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
